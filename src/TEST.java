c91294aaded8e845e651d3c014364c9f14341fd7
Andrea <amourinodesousa@danielcastelao.com>
2025-05-21 18:20:29 +0200

    TEST

---
+++ src/TEST.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+
+public class TEST {
+    @Test
+    void cambiarVelocidadShouldThrowExceptionIfCocheDoesNotExist() {
+    }
+
+    @Test
+    void getCoche() {
+    }
+
+    @Test
+    void getVelocidadShouldReturnCorrectVelocidad() {
+    }
+
+    @Test
+    void crearCoche() {
+    }
+
+    @Test
+    void cambiarVelocidadShouldUpdateCocheVelocidad() {
+    }
+    @Test
+    void getTodosLosCochesShouldReturnAllCoches() {
+    }
+    @Test
+    void addCocheShouldAddCocheToParking() {
+    }
+    @Test
+    void eliminarCocheShouldRemoveCocheFromParking() {
+    }
+    @Test
+    void mostrarDetallesCocheShouldShowDetailsOfCoche() {
+    }
+    @Test
+    void resetearVelocidadShouldResetCocheVelocidad() {
+    }
+}
